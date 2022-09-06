eaac1366cafbaa4bab775990acaefcabf9314818
choiliv <104757153+choiliv@users.noreply.github.com>
2022-09-06 19:44:10 +0900

    #4.4~4.6

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
+
+
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting= document.querySelector("#greeting");
 
-function onLoginSubmit(tomato) {
-   tomato.preventDefault();
-   console.log(tomato);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+   event.preventDefault();
+   loginForm.classList.add(HIDDEN_CLASSNAME);
+   const username = loginInput.value;
+   localStorage.setItem(USERNAME_KEY,username);
+   paintGreeting(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+function paintGreeting (username) {
+   greeting.innerText = `Hello ${username}`;
+   greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+   paintGreeting(savedUsername);
+   greeting.classList.remove(HIDDEN_CLASSNAME);
+}
